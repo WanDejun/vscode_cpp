468647e8aae8cfc42da6cf4116e2b3e7f7cae1dd
wandejun <wandejun666@gmail.com>
2023-10-31 23:01:08 +0800

    add snake game

---
+++ HEU_class/snake_pro.c
@@ -0,0 +1,257 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <Windows.h>
+#include <time.h>
+#include <conio.h>
+
+#define LENGTH 25
+#define SLEEP_TIME 100 //难度
+#define BODY 1
+#define CANDY 2
+
+HANDLE hOutput, hOutBuf;//控制台屏幕缓冲区句柄
+COORD coord = { 0,0 };
+//双缓冲处理显示
+COORD dwBufferSize = { LENGTH };
+DWORD bytes = 0;
+CHAR_INFO output_data[(LENGTH + 5) << 1][(LENGTH + 5) << 1];
+bool buf_flag = 0;
+
+typedef struct LOCATION{
+    int x;
+    int y;
+}snake_loc, candy_loc;
+
+snake_loc snake_Head, snake_Tail;
+struct LOCATION mx_next[LENGTH][LENGTH]; //标记后节点
+struct LOCATION mx_pre[LENGTH][LENGTH]; //标记前节点
+bool mx_flag[LENGTH][LENGTH]; //标记蛇身
+candy_loc candy;
+bool flag_game_over = 0;
+
+int dis[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+int key_status = 0; //0:up 1:down 2:left 3:right
+
+int score = 1; //aslo length
+CHAR_INFO outline[(LENGTH + 5) << 1];
+
+void _init_() {
+    system("chcp 437");
+    system("cls");
+    srand(time(0));
+    CHAR_INFO CharInfo;
+
+    CharInfo.Attributes = FOREGROUND_BLUE | BACKGROUND_BLUE;
+    CharInfo.Char.AsciiChar = '*';
+    for (int i = 0; i < ((LENGTH + 2) << 1); i++) outline[i] = CharInfo; //边界字符串初始化
+    
+    for (int i = 0; i < LENGTH; i++) {
+        output_data[i][0] = CharInfo;
+        output_data[i][(LENGTH + 1) << 1 | 1] = CharInfo;
+    }
+
+    CharInfo.Char.AsciiChar = ' ';
+    for (int i = 0; i < LENGTH; i++) {
+        output_data[i][1] = CharInfo;
+        output_data[i][(LENGTH + 1) << 1] = CharInfo;
+    }
+
+
+    snake_Head.x = rand() % LENGTH; //初始化头
+    snake_Head.y = rand() % LENGTH;
+    snake_Tail.x = snake_Head.x; //初始化尾
+    snake_Tail.y = snake_Head.y;
+    mx_flag[snake_Head.x][snake_Head.y] = 1;
+
+    do { //初始化糖果
+        candy.x = rand() % LENGTH; 
+        candy.y = rand() % LENGTH;
+    } while (candy.x == snake_Head.x && candy.y == snake_Head.y);
+
+    //创建新的控制台缓冲区
+    hOutBuf = CreateConsoleScreenBuffer(
+        GENERIC_WRITE,//定义进程可以往缓冲区写数据
+        FILE_SHARE_WRITE,//定义缓冲区可共享写权限
+        NULL,
+        CONSOLE_TEXTMODE_BUFFER,
+        NULL
+    );
+    hOutput = CreateConsoleScreenBuffer(
+        GENERIC_WRITE,//定义进程可以往缓冲区写数据
+        FILE_SHARE_WRITE,//定义缓冲区可共享写权限
+        NULL,
+        CONSOLE_TEXTMODE_BUFFER,
+        NULL
+    );
+    //隐藏两个缓冲区的光标
+    CONSOLE_CURSOR_INFO cci;
+    cci.bVisible = 0;
+    cci.dwSize = 1;
+    SetConsoleCursorInfo(hOutput, &cci);
+    SetConsoleCursorInfo(hOutBuf, &cci);
+
+}
+
+void fill_in_data() {
+    int x, y, xx, yy;
+    CHAR_INFO CharInfo;
+    CharInfo.Attributes = FOREGROUND_BLUE | BACKGROUND_BLUE;
+    CharInfo.Char.AsciiChar = ' ';
+    for (x = 0; x < LENGTH; x++) {
+        for (y = 1; y <= LENGTH; y++) {
+            output_data[x][y << 1] = CharInfo;
+            output_data[x][y << 1 | 1] = CharInfo;
+        }
+    }
+    for (x = snake_Head.x, y = snake_Head.y; 1; xx = mx_next[x][y].x, yy = mx_next[x][y].y, x = xx, y = yy) {
+        if (x == snake_Head.x && y == snake_Head.y) CharInfo.Attributes = FOREGROUND_RED | BACKGROUND_RED; //设置蛇头颜色
+        else CharInfo.Attributes = FOREGROUND_GREEN | BACKGROUND_GREEN; //身体颜色
+        CharInfo.Char.AsciiChar = 219;
+        output_data[x][(y + 1) << 1] = CharInfo;
+        output_data[x][(y + 1) << 1 | 1] = CharInfo;
+        
+        if (x == snake_Tail.x && y == snake_Tail.y) break;
+    }
+    CharInfo.Attributes = FOREGROUND_BLUE | BACKGROUND_BLUE;
+    CharInfo.Char.AsciiChar = '#';
+    output_data[candy.x][(candy.y + 1) << 1] = CharInfo;
+    output_data[candy.x][(candy.y + 1) << 1 | 1] = CharInfo;
+
+}
+
+void show() {
+    HANDLE *out_buf = &hOutBuf;
+    int i;
+    char str[15];
+    sprintf(str, "score: %3d", score);
+    if (buf_flag) out_buf = &hOutput;
+
+    coord.Y = 0;
+    WriteConsoleOutput(*out_buf, outline, (LENGTH + 2) << 1, coord, &bytes);
+    for (i = 0; i < LENGTH; i++) {
+        coord.Y = i + 1;
+        WriteConsoleOutputCharacterA(*out_buf, output_data[i], (LENGTH + 2) << 1, coord, &bytes);
+    }
+    coord.Y++;
+    WriteConsoleOutputCharacterA(*out_buf, outline, (LENGTH + 2) << 1, coord, &bytes);
+    coord.Y++;
+    WriteConsoleOutputCharacterA(*out_buf, str, 10, coord, &bytes);
+    //设置新的缓冲区为活动显示缓冲
+    SetConsoleActiveScreenBuffer(*out_buf);
+
+
+    Sleep(SLEEP_TIME);
+    buf_flag = !buf_flag;
+}
+
+void getkey() {
+    char ch;
+    if (_kbhit()){ //如果有按键按下，则_kbhit()函数返回真 w:119 a:97 s:115 d:100 esc:27
+        ch = _getch(); //使用_getch()函数获取按下的键值
+        switch (ch) {
+        case 119:
+            if (key_status != 1 || score == 1) key_status = 0;
+            break;
+        case 115:
+            if (key_status != 0 || score == 1) key_status = 1;
+            break;
+        case 97:
+            if (key_status != 3 || score == 1) key_status = 2;
+            break;
+        case 100:
+            if (key_status != 2 || score == 1) key_status = 3;
+            break;
+        case 27:
+            system("pause");
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+bool check_cycle_die(int xx, int yy) {
+    return mx_flag[xx][yy];
+}
+
+void move_snake() {
+    int xx = snake_Head.x + dis[key_status][0], yy = snake_Head.y + dis[key_status][1]; //head前移
+
+    if (xx < 0 || yy < 0 || xx >= LENGTH || yy >= LENGTH) {flag_game_over = 1; return;} //判断越界
+
+    if (check_cycle_die(xx, yy)) { flag_game_over = 1;return; } //撞上自己
+
+    mx_pre[snake_Head.x][snake_Head.y].x = xx; //修改头指针
+    mx_pre[snake_Head.x][snake_Head.y].y = yy;
+
+    mx_next[xx][yy].x = snake_Head.x;
+    mx_next[xx][yy].y = snake_Head.y;
+
+    snake_Head.x = xx; //修改头
+    snake_Head.y = yy;
+
+    mx_flag[xx][yy] = 1;
+
+    if (snake_Head.x == candy.x && snake_Head.y == candy.y) { //吃到糖果
+        do {
+            candy.x = rand() % LENGTH;
+            candy.y = rand() % LENGTH;
+        } while (mx_flag[candy.x][candy.y]);
+        score++;
+    }
+    else {
+        xx = mx_pre[snake_Tail.x][snake_Tail.y].x; //修改尾
+        yy = mx_pre[snake_Tail.x][snake_Tail.y].y;
+
+        snake_Tail.x = xx;
+        snake_Tail.y = yy;
+        mx_flag[xx][yy] = 0;
+    }
+}
+
+void output_die() {
+    HANDLE *out_buf = &hOutBuf;
+    int i;
+    if (buf_flag) out_buf = &hOutput;
+
+    for (int i = 0; i < (LENGTH + 3) << 1; i++)
+        outline[i] = ' ';
+
+    for (int i = 0; i < LENGTH + 2; i++) {
+        coord.Y = i;
+        WriteConsoleOutputCharacterA(*out_buf, outline, (LENGTH + 3) << 1, coord, &bytes);
+    }
+    char str[5][120] = {"    ******       *          *     *        *******          ******    **     **   *******   ******  ",
+                        "   **           * *        * *   * *       **              **    **    *     *    **        *     * ",
+                        "  **   ****    *****      *   * *   *      *******        **      **    *   *     *******   ******  ",
+                        "   **    **   *     *    **    *    **     **              **    **      * *      **        * **    ",
+                        "    ***** *  ***   ***  ***    *    ***    *******          ******        *       *******   *   *** ",};
+    for (i = 0; i < 5; i++) {
+        coord.Y = i;
+        WriteConsoleOutputCharacterA(*out_buf, str[i], 100, coord, &bytes);
+    }
+    SetConsoleActiveScreenBuffer(*out_buf);
+    Sleep(3000);
+    buf_flag = !buf_flag;
+}
+
+void game_start() {
+    fill_in_data();
+    show();
+    system("pause");
+    while (!flag_game_over) {
+        getkey();
+        move_snake();
+        fill_in_data();
+        show();
+    }
+    output_die();
+}
+
+int main() {
+    _init_();
+    game_start();
+
+    return 0;
+}
--- test1.cpp
+++ test1.cpp
@@ -1,11 +1,58 @@
 #include <stdio.h>
-#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+#include <Windows.h>
+#include <time.h>
+#include <conio.h>
+#define LENGTH 15
+HANDLE hOutput, hOutBuf;//控制台屏幕缓冲区句柄
+COORD coord = { 0,0 };
+//双缓冲处理显示
+DWORD bytes = 0;
+char a[LENGTH + 1] = "---------------", b[LENGTH + 1] = "***************";
+bool buf_flag = 0;
+
+void show() {
+    int i, j;
+    HANDLE *p = &hOutBuf;
+    char *s = a;
+    if (buf_flag) {p = &hOutput;s = b;}
+    for (i = 0; i < LENGTH; i++) {
+        coord.Y = i;
+        WriteConsoleOutputCharacterA(*p, s, LENGTH, coord, &bytes);
+    }
+    SetConsoleActiveScreenBuffer(*p);
+
+    printf("123");
+    
+    Sleep(1000);
+    buf_flag = !buf_flag;
+}
 
 int main() {
-    char str[100];
-    scanf("%s", str);
-    char c = getchar();
-    if (c == '\n' || c == '\0') printf("1\n");
-    system("pause");
-    return 0;
+    //创建新的控制台缓冲区
+    hOutBuf = CreateConsoleScreenBuffer(
+        GENERIC_WRITE,//定义进程可以往缓冲区写数据
+        FILE_SHARE_WRITE,//定义缓冲区可共享写权限
+        NULL,
+        CONSOLE_TEXTMODE_BUFFER,
+        NULL
+    );
+    hOutput = CreateConsoleScreenBuffer(
+        GENERIC_WRITE,//定义进程可以往缓冲区写数据
+        FILE_SHARE_WRITE,//定义缓冲区可共享写权限
+        NULL,
+        CONSOLE_TEXTMODE_BUFFER,
+        NULL
+    );
+    //隐藏两个缓冲区的光标
+    CONSOLE_CURSOR_INFO cci;
+    cci.bVisible = 0;
+    cci.dwSize = 1;
+    SetConsoleCursorInfo(hOutput, &cci);
+    SetConsoleCursorInfo(hOutBuf, &cci);
+    while (1)
+    {
+        show();
+    }
 }
